522cea969a59bbf29dca39eeeb8eb453e6677072
stian.larsen <stian.larsen@sparebank1.no>
2024-09-13 09:12:30 +0200

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,11 +2,17 @@ import { SUB_URL } from "@/utils/constants";
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const staticRoutes: MetadataRoute.Sitemap = [""].map((route) => ({
+  const staticRoutes: MetadataRoute.Sitemap = [
+    "",
+    "learn-more",
+    "about-us",
+    "contact",
+    "faq",
+  ].map((route) => ({
     url: `${SUB_URL}/${route}`,
     lastModified: new Date().toISOString(),
     priority: 1,
-    changeFrequency: "monthly",
+    changeFrequency: "weekly",
   }));
 
   return [...staticRoutes];
